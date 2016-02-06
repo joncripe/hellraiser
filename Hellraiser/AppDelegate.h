5d9bddde7de21ca58a53d1d4d02a9b5a43829e2a
Jon Cripe <joncripe16@gmail.com>
2016-02-06 11:41:22 -0600

    Initial Commit

---
+++ Hellraiser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hellraiser
+//
+//  Created by Jon on 2/6/16.
+//  Copyright © 2016 Smooth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hellraiser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hellraiser
+//
+//  Created by Jon on 2/6/16.
+//  Copyright © 2016 Smooth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
